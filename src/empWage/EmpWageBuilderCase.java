78f9ddb740ed43d7a6349a183c8e69adafebd029
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-04-30 14:34:50 +0530

    ADDED: EmpWage Project Added

---
+++ src/empWage/EmpWageBuilderCase.java
@@ -0,0 +1,28 @@
+package empWage;
+
+public class EmpWageBuilderCase {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HR = 20;
+
+	public static void main(String[] args) {
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+				break;	
+		}
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("EmpWage: " + empWage);
+	}
+
+}
