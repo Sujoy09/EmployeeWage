c7f1c839f2e93053e171550ab1ca62b956dea477
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-04-30 14:48:17 +0530

    Added: added UC2 code

--- src/empWage/EmpWageBuilder.java
+++ src/empWage/EmpWageBuilder.java
@@ -1,20 +1,17 @@
 package empWage;
 
-public class EmpWageBuilderIf {
+public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 		//Constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
+		int IS_FULL_TIME = 1;
 		int EMP_RATE_PER_HR = 20;
 		//Variables
 		int empHrs = 0;
 		int empWage = 0;
 		//Computation
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-		else if (empCheck == IS_FULL_TIME)
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
 		else
 			empHrs = 0;
